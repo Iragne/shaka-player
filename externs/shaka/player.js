5725bd7c77db698555bcd5d5c0b64c591f90449f
Iragne <adelskott@gmail.com>
2024-03-08 12:23:12 +0100

    Feat: update the dash manifest update time

--- externs/shaka/player.js
+++ externs/shaka/player.js
@@ -879,7 +879,8 @@ shaka.extern.xml.Node;
  *   sequenceMode: boolean,
  *   enableAudioGroups: boolean,
  *   multiTypeVariantsAllowed: boolean,
- *   useStreamOnceInPeriodFlattening: boolean
+ *   useStreamOnceInPeriodFlattening: boolean,
+ *   updatePeriod: number
  * }}
  *
  * @property {string} clockSyncUri
@@ -957,6 +958,8 @@ shaka.extern.xml.Node;
  *   but may raise issues if manifest does not have stream consistency
  *   between periods.
  *   Defaults to <code>false</code>.
+ * @property {number} updatePeriod
+ *   Override the update time for the manifest
  * @exportDoc
  */
 shaka.extern.DashManifestConfiguration;
